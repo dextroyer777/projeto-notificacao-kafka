74e8f76efce55b73c353b125d1159f3a992c6f69
Filipe Gomes <gomes.processamento@gmail.com>
2026-03-28 12:30:03 -0300

    correcao no controller

--- src/main/java/notificacao/controller/NotificacaoController.java
+++ src/main/java/notificacao/controller/NotificacaoController.java
@@ -1,19 +1,23 @@
 package notificacao.controller;
 
+import lombok.RequiredArgsConstructor;
 import notificacao.dto.NotificacaoDTO;
 import notificacao.service.KafkaProducerService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notificacoes")
 @RequiredArgsConstructor
 public class NotificacaoController {
-    private final KafkaProducerService producer;
+
+    private final KafkaProducerService producerService;
 
     @PostMapping
-    public String enviar(@RequestBody NotificacaoDTO dto) {
-        producer.enviar(dto);
-        return "Mensagem enviada!";
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<String> enviar(@RequestBody NotificacaoDTO dto) {
+        producerService.enviar(dto);
+        return ResponseEntity.accepted().body("Mensagem enviada!");
     }
 }
